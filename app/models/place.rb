9a0585b3f9bf349537c28277b530e68a3d1cb19e
Jeremy Park <jeremyjpark@Jeremys-MacBook-Pro-4.local>
2015-02-07 19:26:37 -0800

    s

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,8 @@
 class Place < ActiveRecord::Base
   validates :title, :price, :preference, :start_date, :laundry, :agreement, :desc, :address, :user_id, presence: true
-  belongs_to :user
+  
+  belongs_to(
+    :user,
+    class_name: "User"
+  )
 end
