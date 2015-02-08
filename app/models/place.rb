13efc0873f6d06ea89dd416cf7a580b5c2107cc5
Jeremy Park <jeremyjpark@Jeremys-MacBook-Pro-4.local>
2015-02-07 19:02:54 -0800

    added basic associations

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,4 @@
 class Place < ActiveRecord::Base
-  validates :title, :price, :preference, :start_date, :laundry, :agreement, :desc, :address, presence: true
+  validates :title, :price, :preference, :start_date, :laundry, :agreement, :desc, :address, :user_id, presence: true
 
-  belongs_to :user
 end
