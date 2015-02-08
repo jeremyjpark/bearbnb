48743c02e899b0a715d9c5a6af1f3be652c57bf7
Jeremy Park <jeremyjpark@Jeremys-MacBook-Pro-4.local>
2015-02-07 18:50:50 -0800

    removed a line

--- app/controllers/api/places_controller.rb
+++ app/controllers/api/places_controller.rb
@@ -2,7 +2,7 @@ module Api
   class Api::PlacesController < ApplicationController
     def create
       @place = Place.new(place_params)
-      @place["user_id"] = current_user.id
+      # @place["user_id"] = current_user.id
 
       if @place.save
         flash.now[:success] = ["New Listing Created!"]
